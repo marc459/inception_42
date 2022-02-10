5a6b24aeab25539ce482ccaa60fa885cbd6f9490
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2022-02-10 18:37:58 +0100

    all conects toghether properly

--- srcs/data/wordpress/wp-config.php
+++ srcs/data/wordpress/wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'msantos' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'admin' );
+define( 'DB_PASSWORD', 'msantospass' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
