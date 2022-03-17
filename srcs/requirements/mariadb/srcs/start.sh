5be379e85fafbf1bc751839832980c7d49b810a4
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2022-03-17 22:21:46 +0100

    changed mysql root password. INCEPTION ALready finishedgit add --all

--- srcs/requirements/mariadb/srcs/start.sh
+++ srcs/requirements/mariadb/srcs/start.sh
@@ -4,7 +4,9 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
     mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';" &&\
 	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" &&\
-	mysql -e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';" && \
+    mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');" && \
+    #mysql -e "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" &&\
+	#mysql -e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';" && \
     mysql -e "FLUSH PRIVILEGES;"
     service mysql stop
 fi
